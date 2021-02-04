17df0933cba35c40194893e6ae5c2a33f9b92ee0
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-02-04 20:10:36 +0500

    Создана Модел приложении

---
+++ ExampleApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExampleApp.ViewModel.Base;
+
+namespace ExampleApp.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string title;
+
+        public string Title
+        {
+            get => title;
+            set => Set(ref title, value);
+
+
+
+
+
+        }
+    }
+    
+}
