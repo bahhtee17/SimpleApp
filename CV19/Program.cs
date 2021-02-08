ad3067b43ab0e409c7b08bf9fe25858d562de274
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-02-08 17:08:24 +0500

    извлечения данных из сайта Johns Hopkins

---
+++ CV19/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Globalization;
+
+namespace CV19
+{
+    class Program
+    {
+
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+        private static async Task<Stream> GetDataStream()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
+            while (!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+                if (string.IsNullOrEmpty(line)) continue;
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+
+        static void Main(string[] args)
+        {
+
+            var dates = GetDates();
+            Console.WriteLine(string.Join("\r\n", dates));
+
+
+            //foreach(var data_line in GetDataLines())
+            //{
+            //    Console.WriteLine(data_line);
+            //}
+
+
+            Console.ReadKey();
+        }
+    }
+}
