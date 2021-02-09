e30089ca1cfd4df5802c15c06546b695a8c17e67
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-02-09 17:35:13 +0500

    Тестовая предметная область

---
+++ ExampleApp/Models/Decanat/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp.Models.Decanat
+{
+   internal class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+        public double Rating { get; set; }
+
+
+    }
+
+    internal class Group
+    {
+
+        public string Name { get; set; }
+        ICollection<Student> Students { get; set; } 
+    }
+}
